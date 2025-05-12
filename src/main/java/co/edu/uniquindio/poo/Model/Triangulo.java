dc77b17fa9a65112c69e0ea8d242eb13c3a2b1d1
SebastianAndresArevalo <sebastiana.arevalop@uqvirtual.edu.co>
2025-05-11 23:35:21 -0500

    Triangulo.java

--- src/main/java/co/edu/uniquindio/poo/Model/Triangulo.java
+++ src/main/java/co/edu/uniquindio/poo/Model/Triangulo.java
@@ -1,4 +1,49 @@
 package co.edu.uniquindio.poo.Model;
 
 public class Triangulo {
+  private int Base;
+    private int Altura;
+    private int lado;
+    
+    public Triangulo(int base, int altura, int lado) {
+        
+        this.Base = base;
+        this.Altura = altura;
+        this.lado = lado;
+        
+    }
+
+    public int getBase() {
+        return Base;
+    }
+
+    public void setBase(int base) {
+        Base = base;
+    }
+
+    public int getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(int altura) {
+        Altura = altura;
+    }
+
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangulo{" +
+                "Base=" + Base +
+                ", Altura=" + Altura +
+                ", lado=" + lado +
+                '}';
+    }
+  
 }
